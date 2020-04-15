fdaf56f3fc744307af1f3291223d5f70e27ccc2c
Tok736 <to_ev@mail.ru>
2020-04-15 23:00:33 +0300

    commented

--- include/Host.h
+++ include/Host.h
@@ -8,34 +8,34 @@
 
 #include "ServerSession.h"
 
-using boost::asio::ip::tcp;
-
-class server {
-private:
-    boost::asio::io_context& io_context_;
-    tcp::acceptor acceptor_;
-
-    void start_accept() {
-        session* new_session = new session(io_context_);
-        acceptor_.async_accept(new_session->socket(),
-                               boost::bind(&server::handle_accept, this, new_session,
-                                           boost::asio::placeholders::error));
-    }
-    void handle_accept(session* new_session, const boost::system::error_code& error) {
-        if (!error) {
-            new_session->start();
-        }
-        else {
-            delete new_session;
-        }
-
-        start_accept();
-    }
-public:
-    server(boost::asio::io_context& io_context, short port) : io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-    {
-        start_accept();
-    }
-};
+//using boost::asio::ip::tcp;
+//
+//class server {
+//private:
+//    boost::asio::io_context& io_context_;
+//    tcp::acceptor acceptor_;
+//
+//    void start_accept() {
+//        session* new_session = new session(io_context_);
+//        acceptor_.async_accept(new_session->socket(),
+//                               boost::bind(&server::handle_accept, this, new_session,
+//                                           boost::asio::placeholders::error));
+//    }
+//    void handle_accept(session* new_session, const boost::system::error_code& error) {
+//        if (!error) {
+//            new_session->start();
+//        }
+//        else {
+//            delete new_session;
+//        }
+//
+//        start_accept();
+//    }
+//public:
+//    server(boost::asio::io_context& io_context, short port) : io_context_(io_context), acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+//    {
+//        start_accept();
+//    }
+//};
 
 #endif //BOOST_ASIO_TEST_HOST_H
--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 
 
 int main() {
-    try {
-        int port = 5512;
-        boost::asio::io_context io_context;
-        server s(io_context, port);
-        io_context.run();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
+//    try {
+//        int port = 5512;
+//        boost::asio::io_context io_context;
+//        server s(io_context, port);
+//        io_context.run();
+//    }
+//    catch (std::exception& e) {
+//        std::cerr << "Exception: " << e.what() << "\n";
+//    }
 
     return 0;
 }
